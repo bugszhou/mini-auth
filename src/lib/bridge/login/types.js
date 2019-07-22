c63350ace0d17ba96ab4a0d2d4674dc38f26934d
bugszhou <bugszhou@outlook.com>
2019-07-22 16:59:38 +0800

    fix: Ch scopes type

--- src/lib/bridge/login/types.js
+++ src/lib/bridge/login/types.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export default function checkOpts(opts = {}) {
   return PropTypes.checkPropTypes({
     timeout: PropTypes.number,
-    scopes: PropTypes.string,
+    scopes: PropTypes.array,
     force: PropTypes.bool,
   }, opts);
 }
